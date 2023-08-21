0071158449a30208ba78660ce822cf7bfa5e83a2
tabu <thangbh1@vmodev.com>
2023-08-21 09:25:53 +0700

    fix: update

--- src/App.js
+++ src/App.js
@@ -20,18 +20,8 @@ import {
 import './App.css'
 
 export default class App extends Component {
-    
   constructor(props) {
     super(props)
-    this.state = {
-      lat: 34.2,
-      lon: -119.2,
-      alt: 10e6,
-      globe: null
-    }
-    // Set the location of the WorldWind "images" folder to the root of the public folder
-    Globe.setBaseUrl("./");    
-    
     this.globeRef = React.createRef()
     this.layersRef = React.createRef()
     this.markersRef = React.createRef()
@@ -39,16 +29,27 @@ export default class App extends Component {
   }
   
   componentDidMount() {
-    // Important! Trigger a state change after the globe is mounted
+    // Get the component with the WorldWindow after mounting
     this.setState({globe: this.globeRef.current})
   }
-  
+
   render() {
-    
     const globe = this.globeRef.current
-    
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    const lat = params.get('lat');
+    const lon = params.get('lon');
+    const alt = params.get('alt');
+    this.state = {
+      lat: parseFloat(lat),
+      lon: parseFloat(lon),
+      alt: alt ? parseInt(alt) : 20e6,
+      globe: null,
+      selectedMarkerImage: 'https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/images/pushpins/castshadow-red.png'
+    }
+
     const layers = [
-      {layer: 'blue-marble', options: {category: 'base', enabled: false}},
+      {layer: 'blue-marble', options: {category: 'base', enabled: true}},
       {layer: 'blue-marble-landsat', options: {category: 'base', enabled: false}},
       {layer: 'bing-aerial', options: {category: 'base', enabled: false}},
       {layer: 'bing-aerial-labels', options: {category: 'base', enabled: false}},
@@ -59,56 +60,49 @@ export default class App extends Component {
       {layer: 'renderables', options: {category: 'data', enabled: true, displayName: 'Markers'}},
       {layer: 'compass', options: {category: 'setting', enabled: false}},
       {layer: 'coordinates', options: {category: 'setting', enabled: true}},
-      {layer: 'view-controls', options: {category: 'setting', enabled: true}},
+      {layer: 'view-controls', options: {category: 'setting', enabled: false}},
       {layer: 'stars', options: {category: 'setting', enabled: false}},
       {layer: 'atmosphere-day-night', options: {category: 'setting', enabled: false}}
     ]
-    
-    const navbarItems = [
-      <NavBarItem key='lyr' title='Layers' icon='list' collapse={this.layersRef.current}/>,
-      <NavBarItem key='mkr' title='Markers' icon='map-marker' collapse={this.markersRef.current}/>,
-      <NavBarItem key='set' title='Settings' icon='cog' collapse={this.settingsRef.current}/>
-    ]
-   
-    const navbarSearch = <SearchBox globe={globe}/>
-    
+
+    const position = {
+      latitude: this.state.lat,
+      longitude: this.state.lon,
+    }
+    //render markup
+    if (globe) {
+      const WorldWind = window.WorldWind
+      let attributes = new WorldWind.PlacemarkAttributes(null);
+      attributes.imageScale = 4.8;
+      attributes.imageOffset = new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 0.3, WorldWind.OFFSET_FRACTION, 0.0);
+      attributes.imageColor = WorldWind.Color.WHITE;
+      attributes.labelAttributes.offset = new WorldWind.Offset(WorldWind.OFFSET_FRACTION, 0.5, WorldWind.OFFSET_FRACTION, 1.0);
+      attributes.labelAttributes.color = WorldWind.Color.YELLOW;
+      attributes.drawLeaderLine = true;
+      attributes.leaderLineAttributes.outlineColor = WorldWind.Color.RED;
+      attributes.imageSource = this.state.selectedMarkerImage;
+
+      let placeMark = new WorldWind.Placemark(position, /*eyeDistanceScaling*/true, attributes);
+      // placeMark.label = "Lat " + position.latitude.toPrecision(4).toString() + "\nLon " + position.longitude.toPrecision(5).toString();
+      placeMark.altitudeMode = WorldWind.CLAMP_TO_GROUND;
+      placeMark.eyeDistanceScalingThreshold = 2500000;
+
+      let layer = globe.getLayer("Markers");
+      if (layer) {
+        layer.addRenderable(placeMark);
+      }
+    }
+
+
     return (
-      <div>
-        <NavBar 
-            logo=''
-            title='WorldWind React Demo'
-            href='https://github.com/emxsys/worldwind-react-app'
-            items={navbarItems}
-            search={navbarSearch} />
-        <Container fluid className='p-0'>
-          <div className='globe'>
-              <Globe 
-                ref={this.globeRef} 
-                layers={layers}/>
-          </div>
-          <div className='overlayTools noninteractive'>
-              <Tools 
-                globe={globe} 
-                markers={this.markersRef.current}
-                markersLayerName='Markers'/>
-          </div>
-          <div className='overlayCards noninteractive'>
-            <CardColumns>
-              <LayersCard
-                ref={this.layersRef}
-                categories={['overlay', 'base']} 
-                globe={globe} />
-              <MarkersCard
-                ref={this.markersRef}
-                globe={globe}
-                markersLayerName='Markers' />
-              <SettingsCard
-                ref={this.settingsRef}
-                categories={['setting']} 
-                globe={globe} />
-            </CardColumns>
-          </div>
-        </Container>
+      <div className='fullscreen'>
+        <Globe
+          ref={this.globeRef}
+          latitude={this.state.lat}
+          longitude={this.state.lon}
+          altitude={this.state.alt}
+          layers={layers}/>
+        />
       </div>
     )
   }
